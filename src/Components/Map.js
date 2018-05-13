2a5c089312fd6b389c59061ffd54ef28b04c0f20
pavlakissimos <paulakis.simos@gmail.com>
2018-05-14 01:30:03 +0300

    Bring new map package and add marker on map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,26 +1,41 @@
 import React, {Component} from "react";
-import {withGoogleMap, GoogleMap} from "react-google-maps";
+import GoogleMapReact from "google-map-react";
 import CommentPath from "./CommentPath";
 
 class Map extends Component {
+	renderMarkers(map, maps) {
+		let marker = new maps.Marker({
+			position: this.props.center,
+			map,
+			title: "Hello World!"
+		});
+	}
+
 	render() {
-		const GoogleMaps = withGoogleMap(props => (
-			<GoogleMap
-				defaultCenter={{lat: 40.756795, lng: -73.954298}}
-				defaultZoom={13}
-			>
-			</GoogleMap>
-		));
 		return (
-			<div className="container map-container">
-				<GoogleMaps
-					containerElement={<div className="map"/>}
-					mapElement={<div style={{height: `100%`}}/>}
-				/>
+			<div className="map-container">
+				<div className="map">
+					<GoogleMapReact
+						bootstrapURLKeys={{key: "AIzaSyBwG6pDtsck7u3pBwRHnv0lga1NOmQRz9Y"}}
+						defaultCenter={this.props.center}
+						defaultZoom={this.props.zoom}
+						onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
+						yesIWantToUseGoogleMapApiInternals
+					>
+					</GoogleMapReact>
+				</div>
 				<CommentPath/>
 			</div>
 		);
 	}
 }
+
+Map.defaultProps = {
+	center: {
+		lat: 37.983810,
+		lng: 23.727539
+	},
+	zoom: 11
+};
 
 export default Map;
