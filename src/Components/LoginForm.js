7e39f067eab20b34afe6798ad77bf59be0757677
pavlakissimos <paulakis.simos@gmail.com>
2018-06-04 03:01:16 +0300

    Add styles in login form

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -4,42 +4,45 @@ class LoginForm extends Component {
   render() {
     return (
       <div className="background-form">
-        <form className="form-container">
-          <div className="form-group">
-            <label htmlFor="emailInput">Email:</label>
-            <input
-              className="form-control"
-              type="email"
-              id="emailInput"
-              name="email"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="usernameInput">Username:</label>
-            <input
-              className="form-control"
-              type="text"
-              id="usernameInput"
-              name="username"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="passwordInput">Password:</label>
-            <input
-              className="form-control"
-              type="password"
-              id="passwordInput"
-              name="password"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <button type="submit" className="btn">Submit</button>
-        </form>
+        <div className="form-container">
+          <p className="logo" />
+          <p className="text-center">Welcome Back</p>
+          <p className="text-muted text-small">Choose Your Path</p>
+          <form>
+            <div className="form-group">
+              <label htmlFor="emailInput">Email:</label>
+              <input
+                className="form-control form-control-lg"
+                type="email"
+                id="emailInput"
+                name="email"
+                autoComplete="off"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="passwordInput">Password:</label>
+              <input
+                className="form-control form-control-lg"
+                type="password"
+                id="passwordInput"
+                name="password"
+                autoComplete="off"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-secondary btn-block">
+              Continue
+            </button>
+            <br />
+            <div className="text-center">Or</div>
+            <br />
+            <button className="btn btn-block btn-facebook">Facebook</button>
+            <button className="btn btn-block btn-google">Google+</button>
+          </form>
+        </div>
       </div>
     );
   }
