d14f23af384ed204450c34cd13207553bb54aed1
pavlakissimos <paulakis.simos@gmail.com>
2018-07-06 02:33:05 +0300

    Add map and form

--- src/Components/NewPath.js
+++ src/Components/NewPath.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import Map from "./Map";
 
 class NewPath extends Component {
   render() {
     return (
-      <div>
-        <div>Add new woander</div>
+      <div className="new-path-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Map />
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="pathName">Path Name:</label>
+                  <input
+                    type="text"
+                    id="pathName"
+                    className="form-control"
+                    autoComplete="off"
+                    placeholder="Woander"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="shortDescription">Short Description:</label>
+                  <textarea className="form-control" id="shortDescription" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
