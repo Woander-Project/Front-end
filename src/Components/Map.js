4aff4f84b0ed1f51a9adacbc0ec68e70ae0a086e
pavlakissimos <paulakis.simos@gmail.com>
2018-07-10 19:26:35 +0300

    Make map get coordinates on click

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
 
 class Map extends Component {
   renderMarkers(map, maps) {
     let marker = [];
     this.props.coordinates.map(
-      (center, i) =>
+      (point, i) =>
         (marker[i] = new maps.Marker({
-          position: center,
+          position: point,
           map,
           title: "Hello World!"
         }))
     );
+  }
+
+  getNewPoint(e) {
+    console.log(e.lat);
+    console.log(e.lng);
   }
 
   render() {
@@ -21,6 +27,7 @@ class Map extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
           onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
+          onClick={this.props.getCoords ? e => this.getNewPoint(e) : null}
           yesIWantToUseGoogleMapApiInternals
         />
       </div>
@@ -29,6 +36,7 @@ class Map extends Component {
 }
 
 Map.defaultProps = {
+  getCoords: false,
   center: {
     lat: 37.98381,
     lng: 23.727539
@@ -38,6 +46,10 @@ Map.defaultProps = {
     { lat: 37.97381, lng: 23.717539 }
   ],
   zoom: 14
+};
+
+Map.propTypes = {
+  getCoords: PropTypes.bool.isRequired
 };
 
 export default Map;
--- src/Components/NewPath.js
+++ src/Components/NewPath.js
@@ -2,13 +2,31 @@ import React, { Component } from "react";
 import Map from "./Map";
 
 class NewPath extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pathName: "",
+      shortDescription: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    e.preventDefault();
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="new-path-container">
         <div className="container">
           <div className="row">
             <div className="col-md-6 col-sm-12">
-              <Map />
+              <Map getCoords={true} />
             </div>
             <div className="col-md-6 col-sm-12">
               <form>
@@ -20,11 +38,16 @@ class NewPath extends Component {
                     className="form-control"
                     autoComplete="off"
                     placeholder="Woander"
+                    onChange={this.handleInputChange}
                   />
                 </div>
                 <div className="form-group">
                   <label htmlFor="shortDescription">Short Description:</label>
-                  <textarea className="form-control" id="shortDescription" />
+                  <textarea
+                    className="form-control"
+                    id="shortDescription"
+                    onChange={this.handleInputChange}
+                  />
                 </div>
               </form>
             </div>
